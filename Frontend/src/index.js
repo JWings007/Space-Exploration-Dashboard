5a79edc49a46e2979827c2a88386627f26984e5d
Varun <115574133+JWings007@users.noreply.github.com>
2023-12-09 17:59:46 +0530

    Update index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
 import App from "./components/App"
+require('dotenv').config();
 const root = createRoot(document.getElementById("app"))
 root.render(<App/>);
